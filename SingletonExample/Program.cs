7cd5f44df05d5b423fdc7b40dbdaa06150fb6f3d
Thiti Yamsung <thiti.y@cdg.co.th>
2016-08-29 13:52:59 +0700

    First commit
    Signed-off-by: Thiti Yamsung <thiti.y@cdg.co.th>

---
+++ SingletonExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample {
+    class Program {
+        static void Main(string[] args) {
+            singletonClass obj = singletonClass.getInstance();
+            Console.WriteLine(obj != null);
+            Console.ReadKey();
+        }
+    }
+
+    public class singletonClass{
+
+        //สร้างตัวแปรสําหรับเก็บ Object ของ class
+        private static singletonClass _instance = null;
+
+        //ให้ constructor เป็น private เพื่อทําให้ไม่สามารถสร้าง Object ได้
+        private singletonClass() { }
+
+        //Function สําหรับสร้าง Object และ return ออกไป
+        //ในกรณีที่มีแล้วให้ return ออกไปเลย
+        public static singletonClass getInstance() {
+
+            if (_instance == null) {
+                _instance = new singletonClass();
+            }
+
+            return _instance;
+        }
+
+    }
+
+}
